20a38818f55b96eeba9a02777a05e2d51e317997
0xsuk <iam0xsuk@gmail.com>
2024-08-22 11:07:54 +0900

    fetch users api

--- front/src/pages/sinki-user-sakusei.tsx
+++ front/src/pages/sinki-user-sakusei.tsx
@@ -1,20 +1,37 @@
 import { useState } from "react"
+import {Api} from "../../openapi/api"
+
+const api = new Api({
+    baseUrl: "http://localhost:3000/api/v1"
+})
 
 export default function SinkiUserSakusei() {
 
-    const [namae, setNamae] = useState("");
+    const [name, setName] = useState("");
+
+    const [role, setRole] = useState("");
+
+    const registerUser = async () => {
+      const res = await api.users.createUser({name, role})
+      console.log(res.data)
+      if (!res.data || !res.data.id) {
+        alert((res.error.message))
+      }
+
+      alert("id is " + res.data.id + " name is " + res.data.name)
+    }
 
     return (
         <>
             <div>
                 <p>新規ユーザー作成画面</p>
                 <p>名前</p>
-                <input value={namae} onChange={(event)=>{setNamae(event.target.value)}}></input>
+                <input value={name} onChange={(event)=>{setName(event.target.value)}}></input>
                 <p>役職</p>
-                <input></input>
+                <input value={role} onChange={(event)=>{setRole(event.target.value)}}></input>
 
                 <div>
-                    <button>作成</button>
+                    <button onClick={registerUser}>作成</button>
                 </div>
                 
             </div>
