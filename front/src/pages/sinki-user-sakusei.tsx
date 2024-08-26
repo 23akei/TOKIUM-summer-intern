7493d74000c1bff062f8512576218b2de50a8ce0
0xsuk <iam0xsuk@gmail.com>
2024-08-26 10:14:50 +0900

    use user id context

--- front/src/pages/sinki-user-sakusei.tsx
+++ front/src/pages/sinki-user-sakusei.tsx
@@ -1,36 +1,40 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import {api} from "../const"
+import { Context } from "../Context.tsx"
 
 export default function SinkiUserSakusei() {
 
-    const [name, setName] = useState("");
+  const {setUserID} = useContext(Context);
+  
+  const [name, setName] = useState("");
 
-    const [role, setRole] = useState("");
+  const [role, setRole] = useState("");
 
-    const registerUser = async () => {
-      const res = await api.users.createUser({name, role})
-      console.log(res.data)
-      if (!res.data || !res.data.id) {
-        alert((res.error.message))
-      }
-
-      alert("id is " + res.data.id + " name is " + res.data.name)
+  const registerUser = async () => {
+    const res = await api.users.createUser({name, role})
+    console.log(res.data)
+    if (!res.data || !res.data.id) {
+      alert((res.error.message))
     }
 
-    return (
-        <>
-            <div>
-                <p>新規ユーザー作成画面</p>
-                <p>名前</p>
-                <input value={name} onChange={(event)=>{setName(event.target.value)}}></input>
-                <p>役職</p>
-                <input value={role} onChange={(event)=>{setRole(event.target.value)}}></input>
+    alert("id is " + res.data.id + " name is " + res.data.name)
+    setUserID(res.data.id)
+  }
 
-                <div>
-                    <button onClick={registerUser}>作成</button>
-                </div>
-                
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+      <p>新規ユーザー作成画面</p>
+      <p>名前</p>
+      <input value={name} onChange={(event)=>{setName(event.target.value)}}></input>
+      <p>役職</p>
+      <input value={role} onChange={(event)=>{setRole(event.target.value)}}></input>
+
+      <div>
+      <button onClick={registerUser}>作成</button>
+      </div>
+      
+      </div>
+      </>
+  )
 }
