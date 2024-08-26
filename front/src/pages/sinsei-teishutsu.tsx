0cea2c1f8ff50fb77fb1f6d260c0d9573a7942fd
0xsuk <iam0xsuk@gmail.com>
2024-08-26 15:21:38 +0900

    未申請と申請済みを同じ画面に表示

--- front/src/pages/sinsei-teishutsu.tsx
+++ front/src/pages/sinsei-teishutsu.tsx
@@ -1,15 +1,21 @@
 import { useContext, useEffect, useState } from "react"
-import {api} from "../const"
-import { Application } from "../../openapi/api";
+import { api } from "../const"
+import { Application, Submittion } from "../../openapi/api";
 import { Context } from "../Context";
 
 
+interface SubmittionAndApplication {
+  submittion: Submittion
+  application?: Application,
+}
+
 export default function SinseiTeishutsu() {
-  const {userID, setUserID} = useContext(Context)
-  const [applications, setApplications] = useState<Application[]>([]);
+  const { userID } = useContext(Context)
+  const [toSubimitList, setToSubmitList] = useState<Application[]>([]); //submitted yet
+  const [submittedList, setSubmittedList] = useState<SubmittionAndApplication[]>([]); //submitted already
   const [selectedApplicationIDs, setSelectedApplicationIDs] = useState<number[]>([]);
 
-  const onSelectApplication = (checked: boolean, application_id : number) => {
+  const onSelectApplication = (checked: boolean, application_id: number) => {
     if (checked) {
       if (!selectedApplicationIDs.includes(application_id)) {
         selectedApplicationIDs.push(application_id);
@@ -23,21 +29,53 @@ export default function SinseiTeishutsu() {
 
   const submitApplications = async () => {
     selectedApplicationIDs.forEach(async (application_id) => {
-      api.submittions.createSubmittion({shinsei_id: application_id, user_id: userID}).then((res: any) => {
+      api.submittions.createSubmittion({ shinsei_id: application_id, user_id: userID }).then((res: any) => {
         console.log(res);
       }).catch((e: any) => {
         console.log(e)
       });
     });
+
+    fetchApplicaions()
   }
 
+  const findSubmittion = (application: Application, submittions: Submittion[]): Submittion | null => {
+    for (let i = 0; i<submittions.length; i++) {
+      if (submittions[i].shinsei_id == application.id) return submittions[i];
+    }
+
+    return null;
+  }
+
+  const classifyApplications = (applications: Application[], submittions: Submittion[]) => {
+    let _submittedList: SubmittionAndApplication[] = []
+
+    setToSubmitList(
+      applications.filter(app => {
+        const sub = findSubmittion(app, submittions)
+        if (sub != null) {
+          _submittedList.push({ submittion: sub, application: app })
+          return false;
+        }
+        return true;
+      })
+    )
+    setSubmittedList(_submittedList)
+  }
 
   const fetchApplicaions = async () => {
-    const res = await api.application.getApplicationsByUserId(userID as number)
-    setApplications(res.data)
+    const resApplications = await api.application.getApplicationsByUserId(userID as number)
+    const resSubmittions = await api.submittions.getSubmittionsByUserId(userID as number)
+
+    if (!resApplications.data || !resSubmittions.data) {
+      alert("yavai!")
+      return
+    }
+
+    classifyApplications(resApplications.data, resSubmittions.data)
   }
 
-  useEffect(() =>  {
+  useEffect(() => {
     if (userID != undefined && !isNaN(userID)) {
       fetchApplicaions()
     }
@@ -46,25 +84,37 @@ export default function SinseiTeishutsu() {
   return (
     <>
       <div>
-      <p>申請提出画面</p>
-      <button onClick={submitApplications}>提出</button>
+        <p>申請提出画面</p>
+        <button onClick={submitApplications}>提出</button>
 
-      {applications.map((application) =>  (
-        <div key={application.id} style={{border: "1px solid black", display: "flex", justifyContent: "space-between"}}>
-          <input
-            type="checkbox"
-            onChange={(e) => {onSelectApplication(e.target.checked, application.id as number)}}
-            key={application.id}
-          />
-          {Object.entries(application).map(([k, value]) => (
-            <div key={k}>
-              <span>{value}</span>
-            </div>
-          ))}
-        </div>
-      ))}
+        <p>未申請</p>
+        {toSubimitList.map((application) => (
+          <div key={application.id} style={{ border: "1px solid black", display: "flex", justifyContent: "space-between" }}>
+            <input
+              type="checkbox"
+              onChange={(e) => { onSelectApplication(e.target.checked, application.id as number) }}
+              key={application.id}
+            />
+            {Object.entries(application).map(([k, value]) => (
+              <div key={k}>
+                <span>{value}</span>
+              </div>
+            ))}
+          </div>
+        ))}
 
+        <p>申請済み</p>
+        {submittedList.map((sub) => (
+          <div key={sub.application?.id} style={{ border: "1px solid black", display: "flex", justifyContent: "space-between" }}>
+            <div>status:{sub.submittion.status}</div>
+            {Object.entries(sub.application as Application).map(([k, value]) => (
+              <div key={k}>
+                <span>{value}</span>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-      </>
+    </>
   )
 }
