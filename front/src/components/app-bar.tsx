1e2aefe456a8b774dceacdd482375cc168840c5c
23akei <tomoya.kanda.2022b@mlab.info>
2024-08-27 12:00:24 +0900

    add user selection

--- front/src/components/app-bar.tsx
+++ front/src/components/app-bar.tsx
@@ -8,15 +8,32 @@ import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import Autocomplete from '@mui/material/Autocomplete';
 import { Link } from 'react-router-dom';
 
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Context } from '../Context.tsx'
+
+import { api } from '../const.ts'
+import { User } from "../../openapi/api.ts"
+import { TextField } from '@mui/material';
 
 // const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function CustomAppBar() {
   const { userID, setUserID } = useContext(Context);
+  const [users, setUsers] = useState<User[]>([]);
+
+  const getAllUsers = async () => {
+    const response = await api.users.getUsers()
+    if (response.data) {
+      setUsers(response.data);
+    }
+  }
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
 
   const link: any = {
     padding: "2px", color: 'inherit', textDecoration: 'none', display: "flex", height: "100%", alignItems: "center", justifyContent:"center"
@@ -43,7 +60,6 @@ export default function CustomAppBar() {
             Kimochi++
           </Typography>
 
-
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             <Button sx={{ m: 1, color: 'white', display: 'block' }}>
               <Link to="/" style={link}>Home</Link>
@@ -69,13 +85,26 @@ export default function CustomAppBar() {
             </Button>
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-
-
-            <span>user id:</span>
-            <input type="number" value={userID} onChange={(event) => setUserID(event.target.valueAsNumber)} />
-
-
+          <Box sx={{ flexGrow: 1, color: 'white'}}>
+            <Autocomplete
+              sx={{ color: 'white' }}
+              options={users}
+              getOptionLabel={(user) => user.name as string}
+              renderInput={(params) => <TextField
+                {...params}
+                placeholder="ユーザ選択"
+                sx={{ color: 'white' }}
+                />
+              }
+              onChange={(_, value) => {
+                if (value) {
+                  setUserID(value.id);
+                } else {
+                  setUserID('');
+                }
+              }}
+              value={users.find((user) => user.id === userID) || null}
+            />
           </Box>
         </Toolbar>
       </Container>
