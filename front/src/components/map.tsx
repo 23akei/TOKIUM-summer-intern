2a4e9eb67331728ae7a1d4980f72018988a172d7
23akei <tomoya.kanda.2022b@mlab.info>
2024-08-27 17:35:21 +0900

    wip: 店舗名取得ができない

--- front/src/components/map.tsx
+++ front/src/components/map.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Map, APIProvider, MapMouseEvent } from '@vis.gl/react-google-maps';
+import { Map, APIProvider, MapMouseEvent, useMapsLibrary } from '@vis.gl/react-google-maps';
 
 const containerStyle: React.CSSProperties = {
   padding: '20px',
@@ -11,8 +11,23 @@ const containerStyle: React.CSSProperties = {
 const GMap: React.FC = () => {
   const [center, setCenter] = useState({ lat: 35.6809591, lng: 139.7673068 });
 
-  const handleMapClick = (event: MapMouseEvent) => {
-    console.log("clicked place id:"+event.detail.placeId);
+  const handleMapClick = async (event: MapMouseEvent) => {
+    const placeId = event.detail.placeId;
+    console.log("clicked place id: "+placeId);
+    if (!placeId) {
+      return;
+    }
+    // get place details
+    const ps = new google.maps.places.PlacesService(event.map);
+    const request = {
+      placeId: placeId,
+      fields: ["name", "formatted_address", "place_id", "geometry"],
+    };
+    ps.getDetails(request, (place, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK && place) {
+        console.log("place name: "+place.name);
+      }
+    });
   }
 
   useEffect(() => {
